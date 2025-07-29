f1cecdbfeb1be5d91aa3b9a25b55e5c95e75c0af
tsega7659 <yeabsirazelalem791@gmail.com>
2025-07-29 21:03:44 +0300

    initial setup

---
+++ taskup/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import './index.css'
+
+function App() {
+ 
+
+  return (
+    <div className='text-red-300'>
+      <h1 className="text-3xl font-bold underline text-center mt-10 text-blue-500">
+        Welcome to TaskUp!
+      </h1>
+    </div>
+  )
+}
+
+export default App
